61ce2648d006e418dd8e75ca0dddad21d8947143
naipes0787 <leandrodavidcosta@gmail.com>
2020-02-19 00:51:08 -0300

    Base project commit

---
+++ src/test/java/com/leandrocosta/soapintegration/SoapIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandrocosta.soapintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
